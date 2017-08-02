6fc20c1de4a8fadff0595381f909b72af8584b1b
afzalkalliyeth <afzal.kalliyeth@calibreft.com.au>
2017-08-02 21:37:20 +1000

    added accounts and mqtt

--- routes/mop.js
+++ routes/mop.js
@@ -4,7 +4,11 @@ var request = require('request').defaults({
     timeout: 60000
 });
 
+var MQT = require('./mqt');
 var Mop = mongoose.model('Mop');
+
+var client_id = '';
+var client_secret = '';
 
 module.exports = {
 
@@ -13,26 +17,66 @@ module.exports = {
         var state = req.query.state;
         console.log('code:' + code + ' state:' + state)
 
+        //MQT.startAndPush('First message to MQTT');
+
         getAccessToken(code, function(err, body) {
             console.log(body);
-//            Mop.create({}, function(err, mop){
-//
-//            });
+            Mop.create({user_id: req.user._id,
+                        access_token: body.access_token,
+                        refresh_token: body.refresh_token,
+                        app_key: state}, function(err, mop){
+
+                            getAccounts(mop.access_token, function(err, body) {
+
+                                //MQT.startAndPush('First message to MQTT');
+                                console.log('accounts are: ' + body)
+
+                            });
+
+                            console.log('mop is: ' + mop);
+                            res.redirect('/applications');
+
+            });
         });
 
         function getAccessToken(code, callback) {
 
-            var form = {grant_type: 'authorization_code', code: code};
-
             var options = {
                 uri: 'https://sandbox.api.macquariebank.io/connect/v1/oauth2/token',
                 headers : {
-                    'client_id': '',
-                    'client_secret': '',
+                    'client_id': client_id,
+                    'client_secret': client_secret,
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
                 method: 'POST',
                 form: {grant_type: 'authorization_code', code: code},
+                json: true
+            };
+
+            request(options, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                    return callback(null, body);
+                } else {
+                    console.log(error);
+                    console.log(response);
+                    return callback(error, null)
+                }
+
+            })
+        }
+
+
+        function getAccounts(access_token, callback) {
+
+            var options = {
+                uri: 'https://sandbox.api.macquariebank.io/connect/v1/accounts',
+                headers : {
+                    'client_id': client_id,
+                    'client_secret': client_secret,
+                    'Authorization': 'Bearer ' + access_token
+                },
+                method: 'GET',
                 json: true
             };
 
---
+++ routes/mqt.js
@@ -0,0 +1,41 @@
+var mqtt = require('mqtt');
+var options = {
+    port: 1111,
+    host: 'mqtt://m12.cloudmqtt.com',
+    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    username: 'xxx',
+    password: 'xxx',
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+};
+
+
+module.exports = {
+
+    startAndPush: function (message) {
+
+        var client = mqtt.connect('mqtt://m12.cloudmqtt.com', options);
+        client.on('connect', function() { // When connected
+            console.log('connected');
+            // subscribe to a topic
+//            client.subscribe('topic1/#', function() {
+//                // when a message arrives, do something with it
+//                client.on('message', function(topic, message, packet) {
+//                    console.log("Received '" + message + "' on '" + topic + "'");
+//                });
+//            });
+
+            // publish a message to a topic
+            client.publish('topic1/#', message, function() {
+                console.log("Message is published");
+                client.end(); // Close the connection when published
+            });
+        });
+
+    }
+
+}
