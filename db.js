5105853474c54ddafea4d9564f24f334dbd7d2f7
Mathias <mathias.faure@gmail.com>
2017-08-04 21:01:19 +1000

    support env var for docker

--- db.js
+++ db.js
@@ -1,4 +1,12 @@
+
+var db_host= process.env.DB_HOST || 'localhost';
+var db_port=  process.env.DB_PORT || 27017;
+var db_name  = process.env.DB_PORT || 'test';
+console.log('db string' +db_host);
+ var mongo_str='mongodb://'+db_host+':'+db_port+'/'+ db_name
+
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://localhost/test'
+
+	url: mongo_str
 }
