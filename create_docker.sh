6c7eeb2ab5df5fb693457dd99deb5b80b1fe81fb
Mathias <mathias.faure@gmail.com>
2017-08-08 18:07:48 +1000

    fixing docker script

--- create_docker.sh
+++ create_docker.sh
@@ -1,4 +1,3 @@
 docker build -t hackathon-nodejs-app .
-docker run -it --rm --name my-running-app hackathon-nodejs-app
-docker tag c8ca0dfd63d8 asia.gcr.io/data-audio-175707/hackathon-nodejs-app
+docker tag hackathon-nodejs-app asia.gcr.io/data-audio-175707/hackathon-nodejs-app
 gcloud docker -- push asia.gcr.io/data-audio-175707/hackathon-nodejs-app
