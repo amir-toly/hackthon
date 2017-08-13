1d604c632f736f1a5661f4431affbc178585af37
mathias.faure@gmail.com <mathias.faure@gmail.com>
2017-08-14 02:53:23 +1000

    showing linked accts

--- routes/applications.js
+++ routes/applications.js
@@ -20,8 +20,10 @@ module.exports = {
             Mop.findOne({ app_key: Applications.app_key },function (err,mops) {
                 if (err) res.render('error', {error: 'getting Applications mop'})
                 if(mops) {
+                    console.log('linked accounts found'+mops);
                     res.render('application', {oneapp: applications, accounts: mops.accounts});
                 }else {
+                    console.log('NO linked accounts');
                     res.render('application', {oneapp: applications, accounts:null});
                 }
             })
@@ -45,6 +47,7 @@ module.exports = {
             if(err) res.render('error', { error: 'Error deleting Applications'});
             Mop.findOneAndRemove({ app_key: Applications.app_key },function (err,mops) {
                 if(err) res.render('error', { error: 'Error deleting Applications mop'});
+                if(mops) console.log('linked accts removed');
                 res.redirect('/applications');
             });
 
